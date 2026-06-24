80103d449bb869dc0c1e81772aca0247c091c9fd
Thomas <tstrade@buffalo.edu>
2026-06-24 15:18:39 -0400

    Fix merge issue with main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdint.h>
+#include "macros.h"
+#include "ssi_driver.h"
+#include "gpio_driver.h"
+#include "led_driver.h"
+
+int main(void)
+{
+    SSI_CONFIGS ssi_mod_confs =
+    {
+         .loopback = DISABLED,
+         .master_slave = MASTER,
+         .clock_source = SYSTEM,
+         .clock_phase = FIRST_EDGE,
+         .polarity = STEADY_STATE_LOW,
+         .frame_format = FREESCALE,
+         .data_size = 16,
+         .bit_rate = 2e6,
+         .prescale_divisor = 2,
+         .transmit_dma = DISABLED,
+         .receive_dma = DISABLED
+    };
+
+    ssi_init (SSI2, PORT_B, &ssi_mod_confs);
+
+    while (1)
+    {
+        send_frame_buffer (SSI2);
+    }
+
+    return 0;
+}
